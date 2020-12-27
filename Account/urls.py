be314b18c42157ac21cd206f67654a02973a95c5
ShrutiSolani <shrutiusolani@gmail.com>
2020-12-27 12:27:50 +0530

    Changed views.py and user authentication

--- Account/urls.py
+++ Account/urls.py
@@ -3,9 +3,9 @@ from django.urls import path,include
 from django.views import *
 
 urlpatterns = [
-    path("login_as_student/",views.login_as_student,name="student_login")
-    path("login_as_teacher/",views.login_as_teacher,name="teacher_login")
-    path("register_as_student/",views.register_as_student,name="student_register")
+    path("login_as_student/",views.login_as_student,name="student_login"),
+    path("login_as_teacher/",views.login_as_teacher,name="teacher_login"),
+    path("register_as_student/",views.register_as_student,name="student_register"),
     path("register_as_tecaher/",views.register_as_teacher,name="teacher_register")
 ]
 
--- Quiz/urls.py
+++ Quiz/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("Account/",include("Account.urls")),
-    path("Quiz/",include("Quiz.urls")),
+    path("course/", views.course_details, name="course_details"),
+    path("topic/", views.topic_details, name="topic_details"),
+    path("Quiz_Details/", views.quiz_details, name="quiz_details"),
+    path("add_question/", views.to_add_question, name="to_add_question"),
+    path("check_answers/", views.check_my_answer, name="check_my_answer"),
+    path("result/", views.final_score, name="final_score"),
 ]
