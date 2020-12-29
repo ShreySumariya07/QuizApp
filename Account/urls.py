6bc4e3d9da7998f90116df47f55279eaa3e036d2
ShrutiSolani <shrutiusolani@gmail.com>
2020-12-29 22:32:56 +0530

    Issue solved

--- Account/urls.py
+++ Account/urls.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path("login/",views.login,name="login_page"),
+    path("login/", views.login, name="login_page"),
+    path("login_confirm", views.login_confirm, name="login_confirm"),
+    path("login/register_confirm", views.register_confirm, name="register_confirm")
+
     #path("login_as_student/",views.login_as_student,name="student_login"),
     #path("login_as_teacher/",views.login_as_teacher,name="teacher_login"),
     #path("register_as_student/",views.register_as_student,name="student_register"),
--- Account/views.py
+++ Account/views.py
@@ -9,14 +9,16 @@ from django.contrib.auth import login, authenticate
 
 
 def login(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
+
 
 def login_confirm(request):
     user = authenticate(username='Email', password='Password')
     if user is not None:
         return render(request, 'index1.html')
     else:
-        return render(request,"index.html")
+        return render(request, "index.html")
+
 
 def register_confirm(request):
     if request.method == "POST":
@@ -27,22 +29,23 @@ def register_confirm(request):
         email = request.POST.get("email")
         phone_no = request.POST.get("phone")
         teacher_student = request.POST.get("radio")
-        if(teacher_student == "student"):
+        print(first_name,teacher_student)
+        if teacher_student == "student":
             b_day = request.POST.get("b_day")
             grade = request.POST.get("grade")
-            age = int((datetime.datetime.now() - b_day).days / 365.25 )
+            age = int((datetime.datetime.now() - b_day).days / 365.25)
             if pass1 == pass2:
                 if User.objects.filter(email=email).exists():
-                    messages.info(request,"Email Taken")
+                    messages.info(request, "Email Taken")
                     return render(request, "index.html")
                 else:
                     user = User.objects.create(first_name = first_name,last_name=last_name,email=email,password=pass1,is_student=True,phone_no=phone_no)
                     student1 = Student.objects.create(user = user,b_day = b_day,age=age,grade = grade)
                     user.save()
                     student1.save()
-                    return redirect("login")
+                    return redirect("Account/login")
             else:
-                messages.info(request,"Password didn't match")
+                messages.info(request, "Password didn't match")
                 return render(request, "index.html")
 
         else:
@@ -59,11 +62,13 @@ def register_confirm(request):
                     teacher1.save()
                     return redirect("login")
             else:
-                messages.info(request,"Password didn't match")
+                messages.info(request, "Password didn't match")
                 return render(request, "index.html")
     else:
-        return render(request,"index.html")
+        return render(request, "index.html")
 
+
+'''
 def regsiter(request):
     if request.method == "POST":
         teacher_student = request.POST.get("radio")
@@ -79,7 +84,7 @@ def regsiter(request):
         return render(request,"homepage.html")
     else:
         return render("test.html")
-'''
+
 
 # Create your views here.
 def login_as_student(request):
